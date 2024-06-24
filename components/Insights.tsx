6aa3f735c8d566262fa39c911d55e08652449497
Mohammed Abed <moa.abed99@gmail.com>
2024-06-24 12:09:29 +0300

    Initial Commit

---
+++ components/Insights.tsx
@@ -0,0 +1,38 @@
+import { motion } from 'framer-motion'
+import { staggerContainer } from '../utils/motion'
+import InsightCard from './InsightCard'
+import TypingText from './TypingText'
+import { insights } from '../data'
+import TitleText from './TitleText'
+import { Insight } from '../data/index.interface'
+
+const Insights: React.FC = (): React.ReactElement => (
+  <section className='sm:p-16 xs:p-8 px-6 py-12 relative z-10'>
+    <motion.div
+      variants={staggerContainer()}
+      initial='hidden'
+      whileInView='show'
+      viewport={{ once: false, amount: 0.25 }}
+      className='2xl:max-w-[1280px] w-full mx-auto flex flex-col'
+    >
+      <TypingText title='| Insight' textStyles='text-center' />
+      <TitleText
+        title={<>Insight about Metaverse</>}
+        textStyles='text-center'
+      />
+      <div className='mt-[50px] flex flex-col gap-[30px]'>
+        {insights.map(
+          (insight: Insight, index: number): React.ReactElement => (
+            <InsightCard
+              key={`insight-${index}`}
+              index={index + 1}
+              {...insight}
+            />
+          )
+        )}
+      </div>
+    </motion.div>
+  </section>
+)
+
+export default Insights
---
+++ components/TypingText.tsx
@@ -0,0 +1,24 @@
+import { motion } from 'framer-motion'
+import { textVariant2 } from '../utils/motion'
+
+interface TitleTextProps {
+  title: React.ReactElement
+  textStyles?: string
+}
+const TitleText: React.FC<TitleTextProps> = (
+  props: TitleTextProps
+): React.ReactElement => {
+  const { title, textStyles } = props
+  return (
+    <motion.h2
+      variants={textVariant2}
+      initial='hidden'
+      whileInView='show'
+      className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+    >
+      {title}
+    </motion.h2>
+  )
+}
+
+export default TitleText
