2bb65f9fd8b78c1a1235fc33239999229ecf1774
Mohammed Abed <63855373+mabedd@users.noreply.github.com>
2024-06-28 16:43:04 +0300

    Fix TypingText

--- components/TypingText.tsx
+++ components/TypingText.tsx
@@ -1,24 +1,25 @@
-import { motion } from 'framer-motion'
-import { textVariant2 } from '../utils/motion'
+import { motion } from "framer-motion";
+import { textVariant2 } from "../utils/motion";
 
 interface TitleTextProps {
-  title: React.ReactElement
-  textStyles?: string
+  title: React.ReactNode;
+  textStyles?: string;
 }
+
 const TitleText: React.FC<TitleTextProps> = (
   props: TitleTextProps
 ): React.ReactElement => {
-  const { title, textStyles } = props
+  const { title, textStyles } = props;
   return (
     <motion.h2
       variants={textVariant2}
-      initial='hidden'
-      whileInView='show'
+      initial="hidden"
+      whileInView="show"
       className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
     >
       {title}
     </motion.h2>
-  )
-}
+  );
+};
 
-export default TitleText
+export default TitleText;
